80f90d2a5fe137c50179a5b3a113353465719aec
arvindpdmn <arvindpadmanabhan@yahoo.com.sg>
2015-09-30 19:33:21 +0530

    Class-based view code for blog app using View class.

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit'),
-    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'),
-    url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^$', views.PostListView.as_view(), name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/edit/$', views.PostEditView.as_view(), name='post_edit'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.PostDetailView.as_view(), name='post_detail'),
+    url(r'^post/new/$', views.PostNewView.as_view(), name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,34 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils import timezone
 from django.contrib.auth.models import User, AnonymousUser
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from django.views.generic import View
 from .models import Post
 from .forms import PostForm
 
-def post_list(request):
-    try:
-        author = User.objects.get(username='admin')
-        posts = Post.objects.filter(published_date__lte=timezone.now(),
-                                    author=author) \
-                            .order_by('-published_date')
-    except ObjectDoesNotExist:
-        posts = None
-    return render(request, 'blog/post_list.html', {'posts': posts})
+class PostListView(View):
+    def get(self, request):
+        try:
+            author = User.objects.get(username='admin')
+            posts = Post.objects.filter(published_date__lte=timezone.now(),
+                                        author=author) \
+                                .order_by('-published_date')
+        except ObjectDoesNotExist:
+            posts = None
+        return render(request, 'blog/post_list.html', {'posts': posts})
+
     
-@login_required
-def post_new(request):
-    if request.method == "POST":
+class PostNewView(View):
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        form = PostForm()
+        return render(request, 'blog/post_edit.html', {'form': form})    
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
@@ -28,27 +38,37 @@ def post_new(request):
                 post.author = User.objects.get(username='admin')
             post.published_date = timezone.now()
             post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
-    else:
-        form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form': form})    
+            return redirect('blog:post_detail', pk=post.pk)
+        else:
+            return render(request, 'blog/post_edit.html', {'form': form})
 
-@login_required
-def post_edit(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    if request.method == "POST":
+
+class PostEditView(View):
+    @method_decorator(login_required)
+    def get(self, request, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        form = PostForm(instance=post)
+        return render(request, 'blog/post_edit.html', {'form': form})    
+
+    @method_decorator(login_required)
+    def post(self, request, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             if not isinstance(request.user, AnonymousUser):
                 post.author = request.user
+            else:
+                post.author = User.objects.get(username='admin')
             post.published_date = timezone.now()
             post.save()
-            return redirect('blog.views.post_detail', pk=post.pk)
-    else:
-        form = PostForm(instance=post)
-    return render(request, 'blog/post_edit.html', {'form': form})
+            #return HttpResponseRedirect('/post/'+str(post.pk))
+            return redirect('blog:post_detail', pk=post.pk)
+        else:
+            return render(request, 'blog/post_edit.html', {'form': form})
 
-def post_detail(request, pk):
-    post = get_object_or_404(Post, pk=pk)
-    return render(request, 'blog/post_detail.html', {'post': post})
+
+class PostDetailView(View):
+    def get(self, request, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        return render(request, 'blog/post_detail.html', {'post': post})
