85e48ab547a605b02d0651c68d7d03937fdbb5a7
arvindpdmn <arvindpadmanabhan@yahoo.com.sg>
2015-10-01 18:35:04 +0530

    Configured PostForm.

--- blog/forms.py
+++ blog/forms.py
@@ -5,4 +5,19 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ('title', 'text',)
+        fields = ('title', 'text',) # '__all__' for all fields
+        labels = {
+            'title': 'Post title'.title(),
+            'text': 'Full text'.title(),
+        }
+        help_texts = {
+            'text': 'An ideal blog post is about 700-1000 words. Avoid technical jargon. Keep it simple.',
+        }
+        error_messages = {
+            'title': {
+                'required': "Title is mandatory.",
+            },
+        }
+        widgets = {
+            'text': forms.Textarea(attrs={'cols': 80, 'rows': 20}),
+        }
