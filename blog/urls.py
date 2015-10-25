7aa6aca40328afd367e88ca6f2ff6e17d94de432
arvindpdmn <arvindpadmanabhan@yahoo.com.sg>
2015-10-25 20:46:35 +0530

    About page done in 2-column layout. Other minor fixes.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,4 @@
-from django.conf.urls import include, url
-from django.views import generic
-from .models import Post
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
@@ -9,6 +7,5 @@ urlpatterns = [
     url(r'^post/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='post_detail'),
     url(r'^post/new/$', views.CreateView.as_view(), name='post_new'),
     url(r'^authors$', views.ListView.as_view(), name='post_list'),
-    url(r'^about$', views.ListView.as_view(), name='post_list'),
     url(r'^topics', views.ListView.as_view(), name='post_list'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views import generic
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+
+    url(r'^about$', generic.TemplateView.as_view(template_name='about.html'), name="about"),
+    
     url(r'', include('blog.urls', namespace='blog')),
 ]
