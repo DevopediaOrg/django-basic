50b73188a81f9beaafae7e123d9cda988bf1a3bc
arvindpdmn <arvindpadmanabhan@yahoo.com.sg>
2015-10-25 16:57:04 +0530

    Deleted food app, which was created as an example earlier. DB name changed.

--- mysite/settings.py
+++ mysite/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'bootstrap_pagination',
     'blog',
-    'food',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -80,7 +79,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'thetechblog.sqlite3'),
     }
 }
 
