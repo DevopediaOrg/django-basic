c6e8536aa60eb3a3d39887564351e6752b40c19a
Arvind Padmanabhan <arvindpdmn@iedf.in>
2015-10-28 18:02:25 +0530

    Added HTML page for 404 error.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, patterns, url
 from django.conf import settings
 from django.contrib import admin
-from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
